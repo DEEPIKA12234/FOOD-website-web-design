98aad9d7e07cb4e42ad6991422a4be0037d2c195
deepika <deepikakadam8848@gmail.com>
2023-04-07 12:45:49 +0530

    first commit

---
+++ final.js
@@ -0,0 +1,33 @@
+/*let menu = document.querySelector("#menubar");
+let navbar= document.querySelector(".navbar");
+
+menu.onclick=() =>{
+    menu.classList.toggle("fa-times")
+    navbar.classList.toggle("active")
+
+}
+
+window.onscroll =() =>{
+    menu.classList.remove("fa-times")
+    navbar.classList.toggle("active")
+
+    if(window.scrollY > 60){
+        document.querySelector('#scroll-up').classList.add('active');
+    
+    }else{
+        document.querySelector('#scroll-up').classList.remove('active');
+
+    }
+
+    function loader(){
+        document.querySelector('..loader-container').classList.add('fade-out');
+    }
+
+    function fadeout(){
+        setInterval(loader, 3000);
+    }
+
+
+}
+
+window.onload=fadeout();*/
